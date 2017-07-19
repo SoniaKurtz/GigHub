717cb182ca739eedf8a1bb035a6d44d87ba04061
Sonia Kurtz <kurtzsonia@gmail.com>
2017-07-19 18:55:16 +0200

    Implement following

---
+++ GigHub/Controllers/FollowingController.cs
@@ -0,0 +1,40 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+	[Authorize]
+    public class FollowingController : ApiController
+    {
+		private ApplicationDbContext _context;
+
+		public FollowingController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		[HttpPost]
+		public IHttpActionResult Follow(FollowingDto dto)
+		{
+			var userId = User.Identity.GetUserId();
+
+			if(_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId))
+			{
+				return BadRequest("You are already following that artist!");
+			}
+
+			var following = new Following
+			{
+				FollowerId = userId,
+				FolloweeId = dto.FolloweeId
+			};
+			_context.Followings.Add(following);
+			_context.SaveChanges();
+
+			return Ok();
+		}
+    }
+}
