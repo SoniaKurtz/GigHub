1a6c4ff0b49b5aca09be8f74efd3c3fcb85364dc
Sonia Kurtz <kurtzsonia@gmail.com>
2017-07-26 17:47:07 +0200

    Refactoring

--- GigHub/Models/Notification.cs
+++ GigHub/Models/Notification.cs
@@ -8,19 +8,38 @@ namespace GigHub.Models
 		public int Id { get; private set; }
 		public DateTime DateTime { get; private set; }
 		public NotificationType Type { get; private set; }
-		public DateTime? OriginalDateTime { get; set; }
-		public string OriginalVaneu { get; set; }
+		public DateTime? OriginalDateTime { get; private set; }
+		public string OriginalVeneu { get; private set; }
 
 		[Required]
 		public Gig Gig { get; private set; }
 
 		protected Notification() {}
 
-		public Notification(NotificationType type, Gig gig)
+	    private Notification(NotificationType type, Gig gig)
 		{
 			Type = type;
 			Gig = gig ?? throw new ArgumentNullException("gig");
 			DateTime = DateTime.Now;
+		}
+
+		public static Notification GigCreated(Gig gig)
+		{
+			return new Notification(NotificationType.GigCreated, gig);
+		}
+
+		public static Notification GigUpdated(Gig newGig, DateTime originalDateTime, string originalVenue)
+		{
+			var notification = new Notification(NotificationType.GigUpdated, newGig);
+			notification.OriginalDateTime = originalDateTime;
+			notification.OriginalVeneu = originalVenue;
+
+			return notification;
+		}
+
+		public static Notification GigCanceled(Gig gig)
+		{
+			return new Notification(NotificationType.GigCanceled, gig);
 		}
 	}
 }
